788094d6e5d9d1e58d5ea4024f6beda2998a6612
JosphatKimani1 <githuthajosphat@gmail.com>
2026-03-19 20:09:53 +0300

    changed user login from email to username

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
 });
